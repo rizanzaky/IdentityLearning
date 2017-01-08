d42341a3937bf9d37eb397394dbf534746a5e987
Rizan Zaky <rizan149@gmail.com>
2017-01-08 11:32:36 +0530

    re initialize

---
+++ IdentityLearning/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace IdentityLearning.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Placeholder", "Placeholder");
+
+            return View(data);
+        }
+    }
+}
---
+++ IdentityLearning/Data_Layer/AppUserManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using IdentityLearning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentityLearning.Data_Layer
+{
+    public class AppUserManager : UserManager<AppUser>
+    {
+        public AppUserManager(IUserStore<AppUser> store) : base(store)
+        {
+        }
+
+        public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options,IOwinContext context)
+        {
+            AppUserDbContext db = context.Get<AppUserDbContext>();
+            AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+
+            return manager;
+        }
+    }
+}
